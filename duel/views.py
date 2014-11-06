aa0501c8d82461cc7255324d1a17574d66f5108c
Rohan Kapoor <rohan@rohankapoor.com>
2014-11-06 12:22:22 -0600

    Implementing basic question view

--- duel/views.py
+++ duel/views.py
@@ -2,10 +2,13 @@ from duel import app, db
 from models import Question
 from flask import render_template
 
+import json
+
 @app.route('/')
 def home():
     return 'Not Implemented'
 
 @app.route('/question/<id>/')
 def question(id):
-    Question.query.filter_by(id=id).one()
+    question = Question.query.filter_by(id=id).first_or_404()
+    return json.dumps(question.to_dict())
